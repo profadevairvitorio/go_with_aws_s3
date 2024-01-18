b217e4b72ec21fd02bdaf0a39380a5432a28bb34
profadevairvitorio <profadevairvitorio@gmail.com>
2024-01-17 23:34:35 -0300

    chore: create main file

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.21.4
 
 require (
 	github.com/aws/aws-sdk-go v1.49.23 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.24.1 // indirect
+	github.com/aws/smithy-go v1.19.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,59 @@
 package main
 
 import (
+	"bytes"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"log"
+	"time"
 )
 
 func main() {
-	// Configurar a sessão do AWS SDK
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		Config: aws.Config{
-			Endpoint: aws.String("http://localhost:4566"),
-			Region:   aws.String("us-east-1"),
-		},
-		Profile: "default", // Perfil AWS
-	}))
+	const bucket = "profadevairvitorio"
+	const key = "arquivo.csv"
+	const region = "us-east-1"
+	const endpoint = "http://s3.localhost.localstack.cloud:4566"
+	const accessKey = "foo"
+	const secretKey = "bar"
 
+	// Initialize a session
+	sess, _ := session.NewSession(&aws.Config{
+		Region:           aws.String(region),
+		Credentials:      credentials.NewStaticCredentials(accessKey, secretKey, ""),
+		S3ForcePathStyle: aws.Bool(true),
+		Endpoint:         aws.String(endpoint),
+	})
+
+	// Create S3 service client
+	svc := s3.New(sess)
+
+	// Create the S3 Bucket
+	_, err := svc.CreateBucket(&s3.CreateBucketInput{
+		Bucket: aws.String(bucket),
+	})
+
+	// Put an object into the bucket
+	_, err = svc.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body: aws.ReadSeekCloser(
+			bytes.NewReader([]byte("add arquivo")),
+		),
+	})
+
+	// Generate Presigned URL
+	req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	urlStr, err := req.Presign(15 * time.Minute)
+
+	if err != nil {
+		log.Println("Failed to sign request", err)
+	}
+
+	log.Println("\n\n--> Presigned URL: ", urlStr)
+	log.Println("\n\n--> You can access the object by GET: \ncurl -X GET \"" + urlStr + "\"")
 }
